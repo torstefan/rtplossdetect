b4fd79c8f076d1653d0d84ef7b829a472f53b4cd
torstefan <torstefan@gmail.com>
2023-11-20 16:56:08 +0100

    Update start.sh

--- rtpgenrcv/start.sh
+++ rtpgenrcv/start.sh
@@ -4,4 +4,4 @@ NIC=A1
 
 export TMPDIR=$(pwd)
 
-tcpdump -i $NIC -w - -U udp port $PORT | stdbuf -oL tshark -i - -w hei.pcap -b filesize:1024 -b files:1 -T fields -e rtp.seq -d udp.port==$PORT,rtp | python2 seqanalyzer.py
+tcpdump -i $NIC -w - -U udp port $PORT | stdbuf -oL tshark -i - -w hei.pcap -b filesize:1024 -b files:1 -T fields -e rtp.seq -d udp.port==$PORT,rtp | python3 seqanalyzer.py
